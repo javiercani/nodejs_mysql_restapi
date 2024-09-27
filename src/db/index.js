d7d0bf2184db8d97b8ba50a1c516b8cc1a0401ab
Javier Caniparoli <javier.caniparoli@gmail.com>
2024-09-26 22:09:25 -0300

    Logs para encontrar error

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 import {createPool} from 'mysql2/promise';
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "../config.js";
 
+console.log(DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER);
+
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
