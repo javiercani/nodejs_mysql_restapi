db0729fcc0b312a77b1a39829383703aa3afed14
Javier Caniparoli <javier.caniparoli@gmail.com>
2024-09-26 21:44:05 -0300

    Try Catch en Ping Pong

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,11 @@
 import {pool} from '../db/index.js';
 
 export const ping = async (req, res)=>{
-    const [result] = await pool.query("SELECT 'Pong' AS result");
-    res.json(result[0]);
+    try {
+      const [result] = await pool.query("SELECT 'Pong' AS result");
+      res.json(result[0]);
+    } catch (error) {
+      res.status(500).json({ message: "An error has ocurred!" });
+    }
+    
 };
