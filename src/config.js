58b1b7ba35c63b693d7c1d3742b9c92b39ead57a
Javier Caniparoli <javier.caniparoli@gmail.com>
2024-09-26 23:11:12 -0300

    Agregar PuertoDB en .env

--- src/config.js
+++ src/config.js
@@ -7,3 +7,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "launion";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "launion123";
 export const DB_DATABASE = process.env.DB_DATABASE || "astrattodb";
+export const DB_PORT = process.env.DB_PORT || 3306;
