b13187f68ba8e1cd27768600db05fe13a298f8a8
Javier Caniparoli <javier.caniparoli@gmail.com>
2024-09-26 22:41:09 -0300

    Agregar Puerto DB

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,18 @@
 import {createPool} from 'mysql2/promise';
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "../config.js";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT,
+} from "../config.js";
 
-console.log(DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER);
+console.log(DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT);
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
+  port: DB_PORT,
   password: DB_PASSWORD,
   database: DB_DATABASE,
 });
